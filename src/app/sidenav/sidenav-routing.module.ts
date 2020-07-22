3f12b87abf2f8499e47b6dd2636b223cf13d8c18
Richard Poulson <contact@richardpoulson.com>
2020-07-22 07:57:32 -0600

    Routing working, new routes

--- src/app/sidenav/sidenav-routing.module.ts
+++ src/app/sidenav/sidenav-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorkHistoryComponent } from '../work-history/work-history.component';
+import { EducationHistoryComponent } from '../education-history/education-history.component';
+import { ProjectHistoryComponent } from '../project-history/project-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'work-history', component: WorkHistoryComponent },
+  { path: 'education-history', component: EducationHistoryComponent },
+  { path: 'project-history', component: ProjectHistoryComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class SidenavRoutingModule { }
